262a0dd1d91c7b689ceb503356bde88e8fb5ab15
mirko <mirko.perillo@gmail.com>
2021-03-23 20:28:02 +0100

    initial commit

---
+++ enigma.go
@@ -0,0 +1,363 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const LETTERS_IN_ALPHABET = 26
+
+var rotorI = rotor{[26]rune{'E', 'K', 'M', 'F', 'L', 'G', 'D', 'Q', 'V', 'Z', 'N', 'T', 'O', 'W', 'Y', 'H', 'X', 'U', 'S', 'P', 'A', 'I', 'B', 'R', 'C', 'J'}, 'R'}
+var rotorII = rotor{[26]rune{'A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E'}, 'F'}
+var rotorIII = rotor{[26]rune{'B', 'D', 'F', 'H', 'J', 'L', 'C', 'P', 'R', 'T', 'X', 'V', 'Z', 'N', 'Y', 'E', 'I', 'W', 'G', 'A', 'K', 'M', 'U', 'S', 'Q', 'O'}, 'W'}
+var rotorIV = rotor{[26]rune{'E', 'S', 'O', 'V', 'P', 'Z', 'J', 'A', 'Y', 'Q', 'U', 'I', 'R', 'H', 'X', 'L', 'N', 'F', 'T', 'G', 'K', 'D', 'C', 'M', 'W'}, 'K'}
+var rotorV = rotor{[26]rune{'V', 'Z', 'B', 'R', 'G', 'I', 'T', 'Y', 'U', 'P', 'S', 'D', 'N', 'H', 'L', 'X', 'A', 'W', 'M', 'J', 'Q', 'O', 'F', 'E', 'C', 'K'}, 'A'}
+var reflectorB = rotor{[26]rune{'Y', 'R', 'U', 'H', 'Q', 'S', 'L', 'D', 'P', 'X', 'N', 'G', 'O', 'K', 'M', 'I', 'E', 'B', 'F', 'Z', 'C', 'W', 'V', 'J', 'A', 'T'}, '0'}
+var reflectorC = rotor{[26]rune{'F', 'V', 'P', 'J', 'I', 'A', 'O', 'Y', 'E', 'D', 'R', 'Z', 'X', 'W', 'G', 'C', 'T', 'K', 'U', 'Q', 'S', 'B', 'N', 'M', 'H', 'L'}, '0'}
+
+type plug struct {
+	a rune
+	b rune
+}
+
+type rotor struct {
+	letters [26]rune
+	notch   rune
+}
+type config struct {
+	steckerboard     [10]plug
+	rotors           [3]rotor
+	reflector        rotor
+	positions        [3]rune
+	doubleRotorsStep bool
+	rings            [3]int
+	debug            bool
+}
+
+type arrayFlags []string
+
+func (a *arrayFlags) Set(value string) error {
+	values := strings.Split(value, ",")
+	for _, v := range values {
+		*a = append(*a, strings.ToUpper(v))
+	}
+	return nil
+}
+
+func (a *arrayFlags) String() string {
+	return strings.Join(*a, ",")
+}
+
+type steckerFlags []plug
+
+func (s *steckerFlags) Set(value string) error {
+	values := strings.Split(value, ",")
+	if len(values)%2 != 0 {
+		return errors.New("Steckerboard options should be even")
+	}
+	for i := 0; i < len(values); i = i + 2 {
+		a := string2rune(values[i])
+		b := string2rune(values[i+1])
+		*s = append(*s, plug{a, b})
+	}
+
+	return nil
+}
+
+func (s *steckerFlags) String() string {
+	var result string
+	for _, pair := range *s {
+		result = result + string(pair.a) + "," + string(pair.b)
+	}
+	return result
+}
+
+func string2rune(s string) rune {
+	r := []rune(strings.ToUpper(s))
+	return r[0]
+}
+
+func validateRotorsFlag(rotors []string) (bool, error) {
+	var validNames = [5]string{"I", "II", "III", "IV", "V"}
+	if len(rotors) != 3 {
+		panic("You have to select 3 rotors")
+	}
+
+	for _, r := range rotors {
+		if !isInArray(r, validNames) {
+			return false, errors.New(fmt.Sprintf("Rotor %s not exist", r))
+		}
+	}
+	return true, nil
+}
+
+func validateReflectorFlag(reflector string) (bool, error) {
+	var validNames = [5]string{"B", "C"}
+	if !isInArray(reflector, validNames) {
+		return false, errors.New(fmt.Sprintf("Reflector %s not exist", reflector))
+	}
+	return true, nil
+}
+
+func validatePositionsFlag(positions []string) (bool, error) {
+	if len(positions) != 3 {
+		panic("You have to select 3 positions")
+	}
+	for _, r := range positions {
+		letter := []rune(r)
+		if !isLetter(letter[0]) {
+			return false, errors.New(fmt.Sprintf("Position %s is not valid", string(letter[0])))
+		}
+	}
+	return true, nil
+}
+
+func isLetter(r rune) bool {
+	return r >= 65 && r <= 90
+}
+
+func validateSteckerboardFlag(stecker []plug) (bool, error) {
+	for _, p := range stecker {
+		if !isLetter(p.a) || !isLetter(p.b) {
+			return false, errors.New(fmt.Sprintf("Not valid value in (%s,%s)", string(p.a), string(p.b)))
+		}
+	}
+	return true, nil
+}
+
+func isInArray(elem string, collection [5]string) bool {
+	for _, e := range collection {
+		if e == elem {
+			return true
+		}
+	}
+	return false
+}
+
+func flagToRotor(flag string) rotor {
+	switch flag {
+	case "I":
+		return rotorI
+	case "II":
+		return rotorII
+	case "III":
+		return rotorIII
+	case "IV":
+		return rotorIV
+	case "V":
+		return rotorV
+	case "B":
+		return reflectorB
+	case "C":
+		return reflectorC
+	default:
+		return rotorI
+	}
+}
+func toConfig(rotors arrayFlags, reflector string, positions arrayFlags, steckerboard steckerFlags, verbose bool) config {
+	debugCfg := verbose
+	var steckerboardCfg [10]plug
+	for idx, p := range steckerboard {
+		steckerboardCfg[idx] = p
+	}
+
+	reflectorCfg := flagToRotor(reflector)
+	rotorsCfg := [3]rotor{flagToRotor(rotors[0]), flagToRotor(rotors[1]), flagToRotor(rotors[2])}
+	positionsCfg := [3]rune{string2rune(positions[0]), string2rune(positions[1]), string2rune(positions[2])}
+	return config{rotors: rotorsCfg, reflector: reflectorCfg, positions: positionsCfg, steckerboard: steckerboardCfg, debug: debugCfg}
+}
+
+func main() {
+	// example of commang flags
+	// --rotors=I,II,IV --reflector=B --positions=A,A,A --verbose --steckerboard=A,B,C,E
+	lastArg := os.Args[len(os.Args)-1]
+	var message string
+	if strings.HasPrefix(lastArg, "--") {
+		message = ""
+	} else {
+		message = lastArg
+	}
+	var rotorsFlag arrayFlags
+	var reflectorFlag *string
+	var positionsFlag arrayFlags
+	var steckerboardFlag steckerFlags
+	var verboseFlag *bool
+
+	flag.Var(&rotorsFlag, "rotors", "comma separated list of rotors. Available values: I,II,III,IV,V. Default I,II,III")
+	reflectorFlag = flag.String("reflector", "B", "reflector rotor to use. Available values: B,C. Default B")
+	flag.Var(&positionsFlag, "positions", "positions of rotors. Default A,A,A")
+	flag.Var(&steckerboardFlag, "steckerboard", "comma separated list of pairs. Optional flag")
+	verboseFlag = flag.Bool("verbose", false, "more verbose output")
+
+	flag.Parse()
+	if rotorsFlag == nil {
+		rotorsFlag = []string{"I", "II", "III"}
+	} else {
+		_, err := validateRotorsFlag(rotorsFlag)
+		if err != nil {
+			panic(err)
+		}
+	}
+	_, err := validateReflectorFlag(*reflectorFlag)
+	if err != nil {
+		panic(err)
+	}
+	if positionsFlag == nil {
+		positionsFlag = []string{"A", "A", "A"}
+	} else {
+		_, err := validatePositionsFlag(positionsFlag)
+		if err != nil {
+			panic(err)
+		}
+	}
+	_, err = validateSteckerboardFlag(steckerboardFlag)
+	if err != nil {
+		panic(err)
+	}
+
+	config := toConfig(rotorsFlag, *reflectorFlag, positionsFlag, steckerboardFlag, *verboseFlag)
+	resultMsg := encrypt(&config, message)
+	fmt.Println(resultMsg)
+}
+
+func applyRotors(cfg *config, letter rune) rune {
+	position := letter - 'A'
+	var encryptedLetter rune
+	for i := len(cfg.rotors) - 1; i >= 0; i-- {
+		rotorPosition := cfg.positions[i] - 'A'
+		encryptedLetter = cfg.rotors[i].letters[((position + rotorPosition) % LETTERS_IN_ALPHABET)]
+		encryptedLetter = ((LETTERS_IN_ALPHABET + encryptedLetter - 'A' - rotorPosition) % LETTERS_IN_ALPHABET) + 'A'
+		position = encryptedLetter - 'A'
+		if cfg.debug {
+			fmt.Println("Rotor step: ", string(encryptedLetter))
+		}
+	}
+	return encryptedLetter
+}
+
+func applyInverseRotors(cfg *config, letter rune) rune {
+	var encryptedLetter rune
+	var position int
+	for i, r := range cfg.rotors {
+		rotorPosition := cfg.positions[i] - 'A'
+		letter = 'A' + ((letter - 'A' + rotorPosition) % LETTERS_IN_ALPHABET)
+		for pos, l := range r.letters {
+			if l == letter {
+				position = pos
+				break
+			}
+		}
+		encryptedLetter = 'A' + ((LETTERS_IN_ALPHABET + rune(position) - rotorPosition) % LETTERS_IN_ALPHABET)
+		letter = encryptedLetter
+		if cfg.debug {
+			fmt.Println("Inverse rotor step: ", string(encryptedLetter))
+		}
+	}
+	return encryptedLetter
+}
+
+func applyReflector(cfg *config, letter rune) rune {
+	position := letter - 'A'
+	return cfg.reflector.letters[position]
+}
+
+func steckerboard(cfg *config, letter rune) rune {
+	encrypted := letter
+	for _, plug := range cfg.steckerboard {
+		if plug.a == letter {
+			encrypted = plug.b
+			break
+		} else if plug.b == letter {
+			encrypted = plug.a
+			break
+		}
+	}
+	return encrypted
+}
+
+func rotorsMovement(cfg *config) {
+	if cfg.doubleRotorsStep {
+		currentPos := cfg.positions[1]
+		cfg.positions[1] = 'A' + (currentPos+1-'A')%LETTERS_IN_ALPHABET
+		currentPos = cfg.positions[0]
+		cfg.positions[0] = 'A' + (currentPos+1-'A')%LETTERS_IN_ALPHABET
+		cfg.doubleRotorsStep = false
+		if cfg.debug {
+			fmt.Println("doubleRotorStep executed")
+		}
+	}
+	currentPos := cfg.positions[2]
+	nextPos := 'A' + (currentPos+1-'A')%LETTERS_IN_ALPHABET
+	cfg.positions[2] = nextPos
+	if cfg.positions[2] == cfg.rotors[2].notch {
+		if cfg.debug {
+			fmt.Println("Rotor position 3 notch")
+		}
+		currentPos = cfg.positions[1]
+		cfg.positions[1] = 'A' + (currentPos+1-'A')%LETTERS_IN_ALPHABET
+		if cfg.positions[1] == cfg.rotors[1].notch {
+			cfg.doubleRotorsStep = true
+			if cfg.debug {
+				fmt.Println("Rotor position 2 notch, doubleRotorStep on next movement")
+			}
+		}
+	}
+}
+
+func encrypt(cfg *config, msg string) string {
+	msg = strings.ToUpper(msg)
+	if cfg.debug {
+		fmt.Println("Input: ", msg)
+	}
+	var encryptedMsg string
+
+	for _, l := range msg {
+
+		if l == ' ' {
+			l = 'X'
+		}
+
+		if l >= 65 && l <= 90 {
+			// 0. rotors movement
+			rotorsMovement(cfg)
+			if cfg.debug {
+				fmt.Println("positions: ", string(cfg.positions[0]), string(cfg.positions[1]), string(cfg.positions[2]))
+			}
+			if cfg.debug {
+				fmt.Println("Input step: ", string(l))
+			}
+			// 1. stecker
+			encryptedLetter := steckerboard(cfg, l)
+			if cfg.debug {
+				fmt.Println("steckerboard encryption: ", string(encryptedLetter))
+			}
+
+			// 2. rotors
+			encryptedLetter = applyRotors(cfg, encryptedLetter)
+			if cfg.debug {
+				fmt.Println("rotors encryption: ", string(encryptedLetter))
+			}
+			// 3. reflector
+			encryptedLetter = applyReflector(cfg, encryptedLetter)
+			if cfg.debug {
+				fmt.Println("reflector encryption: ", string(encryptedLetter))
+			}
+			// 4. inverse rotors
+			encryptedLetter = applyInverseRotors(cfg, encryptedLetter)
+			if cfg.debug {
+				fmt.Println("rotors encryption: ", string(encryptedLetter))
+			}
+			// 5. stecker
+			encryptedLetter = steckerboard(cfg, encryptedLetter)
+			if cfg.debug {
+				fmt.Println("steckerboard encryption: ", string(encryptedLetter))
+			}
+
+			encryptedMsg += string(encryptedLetter)
+		}
+	}
+	return encryptedMsg
+}
---
+++ enigma_test.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestEncodeA(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'A'}}
+	encrypted := applyRotors(&cfg, 'A')
+	if encrypted != 'Z' {
+		t.Error("error: ", string(encrypted))
+	}
+}
+
+func TestEncodeAWithPositionsAAB(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'B'}}
+	encrypted := applyRotors(&cfg, 'A')
+	if encrypted != 'F' {
+		t.Error("error: ", string(encrypted))
+	}
+}
+
+func TestEncodeAWithPositionsAAG(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'G'}}
+	encrypted := applyRotors(&cfg, 'A')
+	if encrypted != 'G' {
+		t.Error("error: ", string(encrypted))
+	}
+}
+
+func TestEncodeInverseRotorsAWithPositionsAAG(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'G'}, debug: false}
+	encrypted := applyInverseRotors(&cfg, 'A')
+	if encrypted != 'H' {
+		t.Error("error: ", string(encrypted))
+	}
+}
+
+func TestEncodeInverseRotorsLWithPositionsAAG(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'G'}, debug: false}
+	encrypted := applyInverseRotors(&cfg, 'L')
+	if encrypted != 'W' {
+		t.Error("error: ", string(encrypted))
+	}
+}
+
+func TestEncodeInverseRotorsAWithPositionsAAB(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'B'}, debug: false}
+	encrypted := applyInverseRotors(&cfg, 'A')
+	if encrypted != 'P' {
+		t.Error("error: ", string(encrypted))
+	}
+}
+
+func TestEncodeB(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'A'}}
+	encrypted := applyRotors(&cfg, 'B')
+	if encrypted != 'N' {
+		t.Error("error: ", encrypted)
+	}
+}
+
+func TestReverseRotor(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'A'}, debug: false}
+	encrypted := applyInverseRotors(&cfg, 'A')
+	if encrypted != 'D' {
+		t.Error("error: ", encrypted)
+	}
+}
+
+func TestReflector(t *testing.T) {
+	cfg := config{reflector: reflectorB, debug: false}
+	encrypted := applyReflector(&cfg, 'A')
+	if encrypted != 'Y' {
+		t.Error("error: ", encrypted)
+	}
+}
+
+func TestSteckerBoard(t *testing.T) {
+	cfg := config{steckerboard: [10]plug{plug{'A', 'Q'}, plug{'R', 'W'}}}
+	encrypted := steckerboard(&cfg, 'Q')
+	if encrypted != 'A' {
+		t.Error("error: ", encrypted)
+	}
+
+}
+
+func TestRotorsMoveInAAA(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'A'}}
+	rotorsMovement(&cfg)
+	newPositions := cfg.positions
+	if differs(newPositions, [3]rune{'A', 'A', 'B'}) {
+		t.Error("error positions: ", string(newPositions[0]), string(newPositions[1]), string(newPositions[2]))
+	}
+}
+
+func TestRotorsMoveInAAG(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'A', 'G'}}
+	rotorsMovement(&cfg)
+	newPositions := cfg.positions
+	if differs(newPositions, [3]rune{'A', 'A', 'H'}) {
+		t.Error("error positions: ", string(newPositions[0]), string(newPositions[1]), string(newPositions[2]))
+	}
+}
+
+func TestRotorsMoveInABC(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'B', 'C'}}
+	rotorsMovement(&cfg)
+	newPositions := cfg.positions
+	if differs(newPositions, [3]rune{'A', 'B', 'D'}) {
+		t.Error("error positions: ", string(newPositions[0]), string(newPositions[1]), string(newPositions[2]))
+	}
+}
+
+func TestRotorsMoveFirstNotch(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, reflector: reflectorB, positions: [3]rune{'A', 'B', 'V'}, debug: false}
+	rotorsMovement(&cfg)
+	newPositions := cfg.positions
+	if differs(newPositions, [3]rune{'A', 'C', 'W'}) {
+		t.Error("error positions: ", string(newPositions[0]), string(newPositions[1]), string(newPositions[2]))
+	}
+}
+
+func TestRotorsMoveDoubleStep(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, positions: [3]rune{'A', 'E', 'V'}}
+	rotorsMovement(&cfg)
+	newPositions := cfg.positions
+	if differs(newPositions, [3]rune{'A', 'F', 'W'}) {
+		t.Error("error positions: ", string(newPositions[0]), string(newPositions[1]), string(newPositions[2]))
+	}
+
+	rotorsMovement(&cfg)
+	newPositions = cfg.positions
+	// double step
+	if differs(newPositions, [3]rune{'B', 'G', 'X'}) {
+		t.Error("error positions: ", string(newPositions[0]), string(newPositions[1]), string(newPositions[2]))
+	}
+
+	rotorsMovement(&cfg)
+	newPositions = cfg.positions
+	// normale step
+	if differs(newPositions, [3]rune{'B', 'G', 'Y'}) {
+		t.Error("error positions: ", string(newPositions[0]), string(newPositions[1]), string(newPositions[2]))
+	}
+}
+
+func TestScenario1(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, reflector: reflectorB, positions: [3]rune{'A', 'A', 'A'}, debug: false}
+	msg := encrypt(&cfg, "enigma")
+	if msg != "FQGAHW" {
+		t.Error("error: ", msg)
+	}
+}
+
+func TestScenario2(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, reflector: reflectorB, positions: [3]rune{'A', 'A', 'A'}, debug: false}
+	msg := encrypt(&cfg, "enigmagoemulator")
+	if msg != "FQGAHWZQWVGRBANF" {
+		t.Error("error: ", msg)
+	}
+}
+
+func TestScenarioWithSteckerboard(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, reflector: reflectorB, positions: [3]rune{'A', 'C', 'H'}, steckerboard: [10]plug{plug{'A', 'Q'}, plug{'R', 'W'}}, debug: false}
+	msg := encrypt(&cfg, "gopherworld")
+	if msg != "SFDXSDGJMJS" {
+		t.Error("error: ", msg)
+	}
+}
+
+func TestScenarioWithNoAlphabeticLetter(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, reflector: reflectorB, positions: [3]rune{'A', 'C', 'H'}, steckerboard: [10]plug{plug{'A', 'Q'}, plug{'R', 'W'}}, debug: false}
+	msg := encrypt(&cfg, "gopher,world!!")
+	if msg != "SFDXSDGJMJS" {
+		t.Error("error: ", msg)
+	}
+}
+
+func TestScenarioWithSpaces(t *testing.T) {
+	cfg := config{rotors: [3]rotor{rotorI, rotorII, rotorIII}, reflector: reflectorB, positions: [3]rune{'A', 'C', 'H'}, steckerboard: [10]plug{plug{'A', 'Q'}, plug{'R', 'W'}}, debug: false}
+	msg := encrypt(&cfg, "super gopher world")
+	if msg != "GNDPGNWJBYUPGTQJGC" {
+		t.Error("error: ", msg)
+	}
+}
+func differs(a [3]rune, b [3]rune) bool {
+	return a[0] != b[0] || a[1] != b[1] || a[2] != b[2]
+}
